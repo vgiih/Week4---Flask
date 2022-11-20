aeb4c4b7035df3ede9a1ad5d06d0674d3de60e8d
vgiih <giihvieira1703@gmail.com>
2022-11-20 18:16:44 +0000

    finally

---
+++ website/routes.py
@@ -0,0 +1,10 @@
+from website import app
+from flask import render_template
+
+@app.route('/')
+def home():
+    return render_template('home.html')
+
+@app.route('/en')
+def english():
+    return render_template('home_en.html')
